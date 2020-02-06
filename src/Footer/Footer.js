efb46b20db11e8861d8aa7e93800c9124239b384
shadurin <igor.shadurin@gmail.com>
2020-02-06 13:30:44 +0300

    added github url to footer

--- src/Footer/Footer.js
+++ src/Footer/Footer.js
@@ -5,9 +5,15 @@ import {Link} from "react-router-dom";
 function Footer({buildDate}) {
     return (
         <footer className="footer text-center text-sm-left">
-            <span className="text-muted">© 2020 Company, Inc. · <Link to="./privacy">Privacy</Link> · <Link
-                to="./terms">Terms</Link> · <Link
-                to="./developers">Developers</Link></span>
+            <span className="text-muted">© 2020 Company, Inc. · <Link to="./privacy">
+                Privacy
+            </Link> · <Link
+                to="./terms">Terms</Link> · <Link to="./developers">
+                Developers
+            </Link> · <a href="https://github.com/GetLoginEth/login" target="_blank">
+                GitHub
+            </a>
+            </span>
             <span className="Footer-build mx-sm-3 float-sm-right text-muted">Build date: {buildDate}</span>
         </footer>
     );
