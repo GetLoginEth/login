605c506d60c7fa724a1c846d0f7bc94bbd3e145f
Igor Shadurin <igor.shadurin@gmail.com>
2020-07-26 16:40:40 +0300

    upgraded logout window

--- src/Logout/Logout.js
+++ src/Logout/Logout.js
@@ -1,16 +1,35 @@
 import React from 'react';
 import './Logout.css';
-import Button from "react-bootstrap/Button";
 import {logoutLocal} from "../reducers/actions";
 
 function Logout() {
     return (
-        <div className="row justify-content-center align-items-center">
-            <div className="border rounded p-4">
-                <h3>Really logout?</h3>
-                <p className="text-muted"><small>Clear local data from your browser</small></p>
-                <Button className="btn-danger mr-1" onClick={logoutLocal} role="button">Yes</Button>
-                <Button className="btn-secondary" onClick={() => window.history.back()} role="button">No</Button>
+        <div className=" " id="logoutModal" tabIndex="-1" role="dialog" aria-labelledby="exampleModalLabel"
+             aria-hidden="true">
+            <div className="modal-dialog" role="document">
+                <div className="modal-content">
+                    <div className="modal-header">
+                        <h5 className="modal-title" id="exampleModalLabel">Ready to Leave?</h5>
+                        {/*<button className="close" type="button" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">×</span>
+                        </button>*/}
+                    </div>
+                    <div className="modal-body">Select "Logout" below if you are ready to clear your local session.
+                    </div>
+                    <div className="modal-footer">
+                        <button className="btn btn-secondary" type="button" data-dismiss="modal" onClick={_ => {
+                            window.history.back();
+                        }}>
+                            Cancel
+                        </button>
+                        <button className="btn btn-primary" type="button" data-dismiss="modal" onClick={e => {
+                            e.preventDefault();
+                            logoutLocal().then();
+                        }}>
+                            Logout
+                        </button>
+                    </div>
+                </div>
             </div>
         </div>
     );
