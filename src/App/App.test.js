505f8ddb8413ee7b494b4a2258a50de9847e0979
Igor Shadurin <igor.shadurin@gmail.com>
2020-08-02 14:34:56 +0300

    prepared for tests

--- src/App/App.test.js
+++ src/App/App.test.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import {act} from 'react-dom/test-utils';
 
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+    act(() => {
+        const div = document.createElement('div');
+
+        ReactDOM.render(<App/>, div);
+        ReactDOM.unmountComponentAtNode(div);
+    });
 });
