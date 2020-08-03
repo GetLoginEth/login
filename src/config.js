217d64ce5f80d50b47685dcabc2600bdb85a4a8b
Igor Shadurin <igor.shadurin@gmail.com>
2020-08-03 23:49:45 +0300

    added mainnet config

--- src/config.js
+++ src/config.js
@@ -3,8 +3,13 @@ export const config = {
         contractAddress: '0x304438f8b26ADE29187B2192E89a2f8cb61E871F',
         websocketProviderUrl: 'wss://rinkeby.infura.io/ws/v3/357ce0ddb3ef426ba0bc727a3c64c873',
         isTrezorEnabled: false
+    },
+    mainnet: {
+        // todo set mainnet contract
+        contractAddress: '',
+        websocketProviderUrl: 'wss://mainnet.infura.io/ws/v3/357ce0ddb3ef426ba0bc727a3c64c873',
+        isTrezorEnabled: false
     }
-    // todo add mainnet config
 };
 
 export function getConfig(network) {
