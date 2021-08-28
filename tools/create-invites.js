e9675635dfc892d69c2e376ca6d58100b4b14bb2
Igor Shadurin <igor.shadurin@gmail.com>
2021-08-28 17:50:44 +0300

    implemented debug bzz mass send

--- tools/create-invites.js
+++ tools/create-invites.js
@@ -3,8 +3,10 @@ const fs = require("fs");
 require('dotenv').config();
 
 const privateKey = process.env.PRIVATE_KEY;
+const tokenAddress = process.env.TOKEN_ADDRESS;
 const dataStorage = JSON.parse(fs.readFileSync('../src/smart/build/contracts/GetLoginStorage.json'));
 const logicStorage = JSON.parse(fs.readFileSync('../src/smart/build/contracts/GetLoginLogic.json'));
+const tokenData = JSON.parse(fs.readFileSync('../src/smart-bzz/build/contracts/Token.json'));
 
 //https://sokol.poa.network
 //77
@@ -21,15 +23,25 @@ const signer = new ethers.Wallet(privateKey).connect(provider);
 const dataAddress = dataStorage.networks[chainId].address;
 // const dataContractInstance = new ethers.Contract(dataAddress, dataStorage.abi, signer);
 const logicAddress = logicStorage.networks[chainId].address;
+const bzzInstance = new ethers.Contract(tokenAddress, tokenData.abi, signer);
 
-const ethHumanPrice = '0.01'; // in reality poa ~0.007 (without bzz transfer)
-const inviteEth = ethers.utils.parseEther(ethHumanPrice);
+const ethInviteHuman = '0.03'; // in reality poa ~0.007 (without bzz transfer and without session creation)
+// 0.01 for FD session balance + FD session creation price??? + ~0.007 to signup without invite + bzz transfer to session price
+const bzzInviteSumHuman = '0.1';
+const ethInvite = ethers.utils.parseEther(ethInviteHuman);
+const bzzInviteSum = ethers.utils.parseUnits(bzzInviteSumHuman, 16);
 
 async function run() {
     let estimateGasLimit;
+    let ethBalance;
+    let bzzBalance;
 
     console.log('chainId', chainId);
     console.log('Signer address', signer.address);
+    ethBalance = await signer.getBalance();
+    bzzBalance = await bzzInstance.balanceOf(signer.address);
+    console.log('Signer ETH/xDai balance', ethers.utils.formatUnits(ethBalance));
+    console.log('Signer BZZ/testBZZ balance', ethers.utils.formatUnits(bzzBalance, 16));
     console.log('dataAddress', dataAddress);
     console.log('logicAddress', logicAddress);
 
@@ -38,13 +50,17 @@ async function run() {
     console.log('gasPrice', gasPrice.toString());
     console.log('Getting logic address...');
     // logicAddress = await dataContractInstance.logicAddress();
+    /**
+     *
+     * @type {ethers.Contract | R}
+     */
     const logicContractInstance = new ethers.Contract(logicAddress, logicStorage.abi, signer);
 
     const addresses = [
-        // '0x52D974Bf887e8A26bACFA39c73E61c35A4299065'
+        '0x52D974Bf887e8A26bACFA39c73E61c35A4299065',
         // '0x80C7062a672585106aE963065BDc6bee60bC8830',
         // '0xE0325606237A4833222b3681a392e372Ec3cd1FF',
-        '0xb6209075afC93a824B6C7398e8fF19cD5248A884',
+        // '0xb6209075afC93a824B6C7398e8fF19cD5248A884',
         // '0x90c3b13DA8a5bd92e08F3441F6D585F26276938d',
         // '0x7E82C2Fa0Bac7928D71C2759E3506F123B19f195',
         // '0x156B9f91F1971a8b7df69f9D7Db40Bfa03BB58aE',
@@ -58,7 +74,7 @@ async function run() {
     ];
     const txParams = {
         // todo calc sum for each invite
-        value: inviteEth,
+        value: ethInvite,
         gasPrice: gasPrice
     };
 
@@ -67,7 +83,7 @@ async function run() {
     console.log('estimateGasLimit', estimateGasLimit.toString());
 
     try {
-        console.log('Creating invites...', ethHumanPrice, inviteEth.toString());
+        console.log('Creating invites...', ethInviteHuman, ethInvite.toString());
         const invites = await logicContractInstance.createInvite(addresses, {
             ...txParams,
             gasLimit: estimateGasLimit
@@ -75,6 +91,17 @@ async function run() {
 
         console.log('Tx sent. Hash', invites.hash);
         const result = await invites.wait();
+
+        // todo this loop isn't ok for mass tx (too slow). Use SWARM practice to send mass ERC20 txs
+        for (let i = 0; i < addresses.length; i++) {
+            const address = addresses[i];
+            console.log(`Approve ${bzzInviteSumHuman} BZZ for ${address}...`);
+            let tx = await bzzInstance.approve(address, bzzInviteSum);
+            await tx.wait();
+            console.log(`Send ${bzzInviteSumHuman} BZZ to ${address}...`);
+            tx = await bzzInstance.transfer(address, bzzInviteSum);
+            await tx.wait();
+        }
         console.log('Complete!');
     } catch (e) {
         console.log('Error while waiting', e);
